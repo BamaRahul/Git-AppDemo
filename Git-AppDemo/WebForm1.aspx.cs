1916d74e560317cb7d20d9664467ea3dfe081f9c
BamaRahul <bama@templecitytech.com>
2018-10-11 13:26:35 +0530

    Chenges

--- Git-AppDemo/WebForm1.aspx.cs
+++ Git-AppDemo/WebForm1.aspx.cs
@@ -4,14 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Git_AppDemo;
 
 namespace Git_AppDemo
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        clsclass obj = new clsclass();
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
+            string s = obj.print();
+            Response.Write(s);
+            
         }
     }
 }
---
+++ Git-AppDemo/clsclass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Git_AppDemo
+{
+    public class clsclass
+    {
+       public string print()
+        {
+            return ("welcome From class");
+        }
+    }
+}
